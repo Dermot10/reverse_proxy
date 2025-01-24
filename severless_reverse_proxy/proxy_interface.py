6e9fb9ba4711f91a8de62f502eff1018f0b6cdf5
Dermot10 <89667760+Dermot10@users.noreply.github.com>
2025-01-24 23:26:16 +0000

    Delete severless_reverse_proxy directory
    renamed

--- severless_reverse_proxy/proxy_interface.py
+++
@@ -1,9 +0,0 @@
-from .rp_request_response import ReverseProxyRequestResponse
-
-
-class ReverseProxyInterface:
-    """Main interface for Reverse Proxy, instantiates individual processes"""
-
-    def __init__(self):
-        # self.target_site = target_site
-        self.reverse_proxy = ReverseProxyRequestResponse()
